a95ef9a087248dd39a83fcd7e977ec5ea2d7ae47
luisrodriguez <codekommissar@gmail.com>
2018-02-26 15:02:27 -0400

    Create production environment

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    redditClientID: process.env.REDDIT_CLIENT_ID,
+    redditClientSecret: process.env.REDDIT_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+};
